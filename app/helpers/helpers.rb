4962438d98ffe261b10444f570c61c532dcf4465
Jess Rezac <jess.rezac@gmail.com>
2019-08-23 20:51:34 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,11 @@
 class Helpers
   def self.current_user(session)
+    binding.pry
+
     User.find(session[:user_id])
   end
 
   def self.is_logged_in?(session)
-    binding.pry
     !!session[:user_id]
   end
 end
