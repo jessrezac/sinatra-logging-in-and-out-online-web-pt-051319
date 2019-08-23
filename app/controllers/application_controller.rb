5334a8f1455ab2bcf8cd81f99aa0fecd55d4c4ff
Jess Rezac <jess.rezac@gmail.com>
2019-08-23 20:05:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(name: params[:name])
+    @user = User.find_by(name: params[:username])
 
     if @user
       session[:user_id] = @user.id
